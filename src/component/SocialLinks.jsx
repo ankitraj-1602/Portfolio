aa82c2a6654e50be4f0a1add25452f8762899dc8
Ankit Raj <nycankit@gmail.com>
2023-03-30 00:03:08 +0530

    resume update

--- src/component/SocialLinks.jsx
+++ src/component/SocialLinks.jsx
@@ -1,3 +1,176 @@
+// // import React from 'react'
+// // import { FaGithub, FaLinkedin } from "react-icons/fa";
+// // import { HiOutlineMail } from "react-icons/hi";
+// // import { BsFillPersonLinesFill } from "react-icons/bs";
+// // import { FaPhone } from 'react-icons/fa';
+
+// // const SocialLinks = () => {
+
+// //   const links = [
+// //     {
+// //       id: 1,
+// //       child: (
+// //         <>
+// //           LinkedIn <FaLinkedin size={30} />
+// //         </>
+// //       ),
+// //       href: 'https://linkedin.com/in/ankitrj1602',
+// //       style: 'rounded-tr-md'
+// //     },
+// //     {
+// //       id: 2,
+// //       child: (
+// //         <>
+// //           GitHub <FaGithub size={30} />
+// //         </>
+// //       ),
+// //       href: 'https://github.com/ankitraj-1602',
+// //       // style: 'rounded-tr-md'
+// //     },
+// //     {
+// //       id: 3,
+// //       child: (
+// //         <>
+// //           mail <HiOutlineMail size={30} />
+// //         </>
+// //       ),
+// //       href: 'mailto:ankitr1602@gmail.com',
+// //       // style: 'rounded-tr-md'
+// //     },
+// //     {
+// //       id: 4,
+// //       child: (
+// //         <>
+// //           Resume <BsFillPersonLinesFill size={30} />
+// //         </>
+// //       ),
+// //       href: './ankitraj_resume.pdf',
+// //       download: true,
+// //     },
+// //     {
+// //       id: 5,
+// //       child: (
+// //         <>
+// //           call   <FaPhone size={30} />
+// //         </>
+// //       ),
+// //       href: 'tel:+919668616234',
+// //       style: 'rounded-br-md',
+// //       download: true,
+// //     },
+// //   ]
+
+// //   return (
+// //     // <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed'>
+// //     <div className='lg:flex flex-col top-[50%] left-0 fixed block sm:hidden'>
+// //       <ul>
+
+
+// //         {links.map(({id, child, href, style, download}) => {
+
+// //          return   <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' + " " + style}>
+// //               <a href={href} className='flex justify-between items-center w-full text-white ' download={download} target='_blank' rel='noreferrer'>
+// //               {child}
+// //             </a>
+// //           </li>
+
+// //         })}
+
+
+// //       </ul>
+
+// //     </div>
+// //   )
+// // }
+
+// // export default SocialLinks
+
+
+
+
+
+
+// import React from 'react'
+// import { FaGithub, FaLinkedin } from "react-icons/fa";
+// import { HiOutlineMail } from "react-icons/hi";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
+// import { FaPhone } from 'react-icons/fa';
+
+// const SocialLinks = () => {
+
+//   const links = [
+//     {
+//       id: 1,
+//       child: (
+//         <>
+//           LinkedIn <FaLinkedin size={30} />
+//         </>
+//       ),
+//       href: 'https://linkedin.com/in/ankitrj1602',
+//       style: 'rounded-tr-md'
+//     },
+//     {
+//       id: 2,
+//       child: (
+//         <>
+//           GitHub <FaGithub size={30} />
+//         </>
+//       ),
+//       href: 'https://github.com/ankitraj-1602',
+//       // style: 'rounded-tr-md'
+//     },
+//     {
+//       id: 3,
+//       child: (
+//         <>
+//           mail <HiOutlineMail size={30} />
+//         </>
+//       ),
+//       href: 'mailto:ankitr1602@gmail.com',
+//       // style: 'rounded-tr-md'
+//     },
+//     {
+//       id: 4,
+//       child: (
+//         <>
+//           Resume <BsFillPersonLinesFill size={30} />
+//         </>
+//       ),
+//       href: './ankit_resume.pdf',
+//       download: true,
+//     },
+//     {
+//       id: 5,
+//       child: (
+//         <>
+//           call   <FaPhone size={30} />
+//         </>
+//       ),
+//       href: 'tel:+919668616234',
+//       style: 'rounded-br-md',
+//       download: true,
+//     },
+//   ]
+
+//   return (
+//     <div className='lg:flex flex-col top-[65%] left-0 fixed block sm:hidden' style={{ backgroundColor: "transparent" }}>
+//       <ul style={{ backgroundColor: "transparent" }}>
+//         {links.map(({id, child, href, style, download}) => {
+//          return   <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' + " " + style}  style={{ backgroundColor: "transparent" }}>
+//               <a href={href} className='flex justify-between items-center w-full text-white ' download={download} target='_blank' rel='noreferrer'>
+//               {child}
+//             </a>
+//           </li>
+//         })}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default SocialLinks
+
+
+
 import React from 'react'
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
@@ -44,7 +217,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: './ankitraj_resume.pdf',
+      href: './ankit_resume.pdf',
       download: true,
     },
     {
@@ -61,24 +234,16 @@ const SocialLinks = () => {
   ]
 
   return (
-    // <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed'>
-    <div className='lg:flex flex-col top-[50%] left-0 fixed block sm:hidden'>
-      <ul>
-
-
+    <div className='lg:flex flex-col bottom-0 fixed block sm:hidden' style={{ backgroundColor: "transparent" }}>
+      <ul style={{ backgroundColor: "transparent" }}>
         {links.map(({id, child, href, style, download}) => {
-
-         return   <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' + " " + style}>
+         return   <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' + " " + style}  style={{ backgroundColor: "transparent" }}>
               <a href={href} className='flex justify-between items-center w-full text-white ' download={download} target='_blank' rel='noreferrer'>
               {child}
             </a>
           </li>
-
         })}
-
-
       </ul>
-
     </div>
   )
 }
